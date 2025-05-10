ce64329d48b2832ee89a8754b4b633574aaa2061
ISHAAN SRIVASTAVA <ishaangaming2002@gmail.com>
2025-05-11 01:09:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+// script.js
+
+// Smooth scroll to section
+const navLinks = document.querySelectorAll('nav ul li a');
+
+navLinks.forEach(link => {
+  link.addEventListener('click', function(e) {
+    e.preventDefault();
+    const targetId = this.getAttribute('href');
+    const targetSection = document.querySelector(targetId);
+    targetSection.scrollIntoView({ behavior: 'smooth' });
+  });
+
+});
+
+
+
+
